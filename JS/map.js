fe93c688e3efe105868b32a2e05b43c73c0462a1
Stephen Gibbs <stephen_gibbs@hotmail.co.uk>
2016-01-07 16:57:56 +0000

    Added google map and footer

---
+++ JS/map.js
@@ -0,0 +1,26 @@
+	var myCenter=new google.maps.LatLng(51.594816, -3.783645);
+		
+	function initialize()
+	{
+	var mapProp = {
+	  center:myCenter,
+	  zoom:7,
+	  mapTypeId:google.maps.MapTypeId.ROADMAP
+	  };
+	  
+	var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+	var myCity = new google.maps.Circle({
+	  center:myCenter,
+	  radius:25000,
+	  strokeColor:"#FFFCD4",
+	  strokeOpacity:0.8,
+	  strokeWeight:2,
+	  fillColor:"#878455",
+	  fillOpacity:0.4
+	  });
+
+	myCity.setMap(map);
+	}
+
+	google.maps.event.addDomListener(window, 'load', initialize);
